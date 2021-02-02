22c5a462b716ff1f56f4b4b0be41172f7db2229c
Ken Dreyer <ktdreyer@ktdreyer.com>
2021-02-02 12:24:21 -0700

    setup.py: update license trove classifier for MIT license (#14)
    Update the trove classifier metadata for this package to indicate that
    it is available under the MIT license. The Apache license appears to be
    a copy-and-paste from elsewhere that does not apply here.

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
         'Environment :: Console',
         'Intended Audience :: Developers',
         'Intended Audience :: Information Technology',
-        'License :: OSI Approved :: Apache Software License',
+        'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
